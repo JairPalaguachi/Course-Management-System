df5dbce7c81ab5eb77d81f31d556958277ce5e48
DhamarPatino <152041769+DhamarPatino@users.noreply.github.com>
2026-06-15 20:44:40 -0500

    feat(auth): implement JWT login authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,12 +137,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 AUTH_USER_MODEL = 'users.User'
---
+++ backend/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import LoginView
+
+urlpatterns = [
+    path(
+        "auth/login/",
+        LoginView.as_view(),
+        name="login"
+    ),
+]

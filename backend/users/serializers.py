e4fa7a7a718a0d36e0695d3e71180fdf414a1afe
Jair Palaguachi <jpalagua@espol.edu.ec>
2026-06-22 21:27:56 -0500

    fix: Some conflicts with Tutor and Student

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -20,7 +20,6 @@ class LoginSerializer(serializers.Serializer):
             )
 
         attrs["user"] = user
-
         return attrs
 
 
@@ -52,7 +51,6 @@ class TutorRegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "Ya existe un usuario con este correo electrónico."
             )
-
         return value
 
     def validate_username(self, value):
@@ -60,7 +58,6 @@ class TutorRegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "Ya existe un usuario con este nombre de usuario."
             )
-
         return value
 
     def validate(self, attrs):
@@ -68,7 +65,6 @@ class TutorRegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({
                 "password_confirm": "Las contraseñas no coinciden."
             })
-
         return attrs
 
     def create(self, validated_data):
@@ -85,3 +81,61 @@ class TutorRegisterSerializer(serializers.ModelSerializer):
 
         return user
 
+
+class StudentRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8,
+        required=True
+    )
+    password_confirm = serializers.CharField(
+        write_only=True,
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+            "password_confirm",
+        ]
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                "Ya existe un usuario con este correo electrónico."
+            )
+        return value
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                "Ya existe un usuario con este nombre de usuario."
+            )
+        return value
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password_confirm"]:
+            raise serializers.ValidationError({
+                "password_confirm": "Las contraseñas no coinciden."
+            })
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+        password = validated_data.pop("password")
+
+        user = User(
+            **validated_data,
+            role=User.Role.STUDENT
+        )
+
+        user.set_password(password)
+        user.save()
+
+        return user
